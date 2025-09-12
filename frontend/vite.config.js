3763ff85ffe3ca8867a925799dcdcf687f27b794
yacentoselmoudene <yassine.elmoudene@gmail.com>
2025-09-12 20:02:09 +0100

    consol in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const target = env.VITE_PROXY_TARGET ;
+  if (env.NODE_ENV === "development") {
+  console.log("Running locally");
+} else {
+  console.log("Running in production");
+}
+   console.log(target + " target ");
   return {
     plugins: [react()],
     server: {
