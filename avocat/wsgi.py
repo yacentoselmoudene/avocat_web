e6e3826bf7a8386dc18c967d462feaa9df8b0aa5
yacentoselmoudene <yassine.elmoudene@gmail.com>
2025-09-15 09:42:13 +0100

    utf8 for print in server

--- avocat/wsgi.py
+++ avocat/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import sys, io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avocat.settings')
 
 application = get_wsgi_application()
